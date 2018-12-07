93fc970389e902bb521219e5b3579e8f22595646
Pavan Hegde <pavanrkadave@gmail.com>
2018-12-07 09:55:01 +0530

    Results Caching bug solved

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HudukkoService } from 'src/app/service/hudukko.service';
 import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-result',
@@ -12,12 +13,14 @@ export class ResultComponent implements OnInit {
   public flipkart = new BehaviorSubject<any>(null);
   public snapdeal = new BehaviorSubject<any>(null);
 
-  constructor(public hudkoService: HudukkoService) {
+  constructor(public hudkoService: HudukkoService, private router: Router) {
     hudkoService.flipkartdata$.subscribe(data => this.flipkart = data);
     hudkoService.snapdealData$.subscribe(data => this.snapdeal = data);
   }
 
   ngOnInit() {
+    this.flipkart = null;
+    this.snapdeal = null;
     console.log(this.flipkart, this.snapdeal);
   }
 
